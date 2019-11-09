ffab9965b2a370eaa5b91f8e5d1bb7971c9e9d9a
Lucas Moraes <lucaszmoraes@gmail.com>
2019-11-09 20:45:13 -0300

    adicionando Bootstrap 4.3.1 via yarn

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,10 +1,14 @@
 class MenuController < ApplicationController
 
+  def index
+
+  end
+
   # Service que recebe a temperatura do endpoint
   # e calcula a melhor cerveja e a retorna com JSON
   def by_temperature
-    byebug
-    given_temperature = beer_by_temperature_params[:temperature].to_i
+    #byebug
+    #given_temperature = beer_by_temperature_params[:temperature].to_i
     given_temperature = 1
     @best_beer = MenuService.new(given_temperature).calculates_best_beer_by_temperature
     render json: @best_beer.to_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
-  # VERB    # PATH                                       # CONTROLLER#METHOD
+  # VERB                # PATH                                      # CONTROLLER#METHOD
   # Routes de Menu  
-  get        '/menu/por-temperatura',     to: "menu#by_temperature"
+  root to: 'menu#index'
+  get                   '/menu/por-temperatura',                    to: "menu#by_temperature"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
