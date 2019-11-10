17260feb71c5ce5e2ea8dade85263f261985d961
Lucas Moraes <lucaszmoraes@gmail.com>
2019-11-09 21:17:26 -0300

    iniciando form para consulta e resultado de calculadora de breja

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -6,12 +6,16 @@ class MenuController < ApplicationController
 
   # Service que recebe a temperatura do endpoint
   # e calcula a melhor cerveja e a retorna com JSON
-  def by_temperature
+  def form_for_beer_by_temperature
     #byebug
     #given_temperature = beer_by_temperature_params[:temperature].to_i
     given_temperature = 1
     @best_beer = MenuService.new(given_temperature).calculates_best_beer_by_temperature
-    render json: @best_beer.to_json
+    #render json: @best_beer
+  end
+
+  def result_of_beer_by_temperature
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
-  # VERB                # PATH                                      # CONTROLLER#METHOD
+  # VERB          # PATH          # CONTROLLER#METHOD
   # Routes de Menu  
   root to: 'menu#index'
-  get                   '/menu/por-temperatura',                    to: "menu#by_temperature"
+  get         '/menu/cerveja-por-temperatura',                  to: "menu#form_for_beer_by_temperature"
+  get         '/menu/resultado/cerveja-por-temperatura',        to: "menu#result_of_beer_by_temperature"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
