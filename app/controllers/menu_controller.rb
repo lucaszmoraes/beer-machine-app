f753115049c3c199f02e5c56abb49933dcf2c6ce
Lucas Moraes <lucaszmoraes@gmail.com>
2019-11-10 11:51:42 -0300

    criando consulta na api

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -4,23 +4,19 @@ class MenuController < ApplicationController
 
   end
 
-  # Service que recebe a temperatura do endpoint
-  # e calcula a melhor cerveja e a retorna com JSON
   def form_for_beer_by_temperature
-    #byebug
-    #given_temperature = beer_by_temperature_params[:temperature].to_i
-    given_temperature = 1
+
+  end
+
+  def result_of_beer_by_temperature
+    given_temperature = beer_by_temperature_params.to_i
     @best_beer = MenuService.new(given_temperature).calculates_best_beer_by_temperature
     #render json: @best_beer
   end
 
-  def result_of_beer_by_temperature
-
-  end
-
   private
     def beer_by_temperature_params
-      params.require(:menu).permit(:temperature)
+      params[:temperature]
     end
 
 end
--- services/menu_service.rb
+++ services/menu_service.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 class MenuService
   attr_reader :given_temperature # necessário pois não há este campo na tabela, ou sequer uma tabela Beers Menu
 
@@ -6,9 +9,30 @@ class MenuService
     @given_temperature = given_temperature
   end
 
+  if false
+    def calculates_best_beer_by_temperature
+      # retorna o ID da melhor cerveja calculada
+      @best_beer = "SKOL"
+      return @best_beer
+    end
+  end
+
   def calculates_best_beer_by_temperature
+    #byebug
+
+    uri = URI.parse("http://localhost:2000/api/v1/beers/menu/melhor-cerveja")
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({"beer[temperature]" => given_temperature})
+    
+    response = http.request(request)
+    #render :json => response.body
+
     # retorna o ID da melhor cerveja calculada
-    @best_beer = "SKOL"
+    #@best_beer = "SKOL"
+    @best_beer = response.body
+    byebug
     return @best_beer
   end
 
